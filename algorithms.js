42deb24dc91071873782b9d8d6502e6472e1f179
Oddur Sigurdsson <oddurs@gmail.com>
2015-03-05 15:58:45 -0500

    added recursion.js

--- algorithms.js
+++ algorithms.js
@@ -1,14 +1,3 @@
-// RECURSION
-function factorial(n) {
-	if (n < 2) return 1;
-	return n * factorial(n-1);
-}
-
-function fibonacci(n) {
-	if (n < 2) return n;
-	return fibonacci(n-1) + fibonacci(n-2);
-}
-
 // SORTING
 function swap(arr, i, j) {
 	var temp = arr[i];
