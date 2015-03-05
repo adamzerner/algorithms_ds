1a2233b18a6c079850c93f65fc676ee34d0d0ac8
Oddur Sigurdsson <oddurs@gmail.com>
2015-03-05 17:04:53 -0500

    added hashTable.js

--- dataStructuresSpec.js
+++ dataStructuresSpec.js
@@ -1,28 +1,3 @@
-describe('A Hash Table', function() {
-  // hash fn uses the number associated with the first letter (eg. a: 1, b: 2...)
-  var h;
-  beforeEach(function() {
-    h = new Hash();
-    h.set('adam', 'me');
-    h.set('jake', 'brother');
-  });
-  it('can get and set', function() {
-    expect(h.get('adam')).toBe('me');
-    expect(h.get('jake')).toBe('brother');
-    expect(h.get('lori')).toBe(undefined);
-  });
-  it('can handle collisions', function() {
-    h.set('alfred', 'grandpa');
-    expect(h.get('alfred')).toBe('grandpa');
-  });
-  it('can overwrite', function() {
-    h.set('adam', 'overwritten');
-    expect(h.get('adam')).toBe('overwritten');
-  });
-  // table resizing
-  // linear probing
-});
-
 // describe("A Binary Heap", function() {
 //   var heap;
 //   var input = [16,14,10,8,7,9,3,2,4,1];
