08ff5741ab1ab5ce1edc40f89991607489b11871
Adam Zerner <azerner3@gmail.com>
2015-11-21 00:06:53 -0500

    BST (todo: #isbst)

---
+++ dataStructures/binarySearchTree/binarySearchTree.js
@@ -0,0 +1,206 @@
+function Node(el) {
+  this.el = el;
+  this.left = { el: null, left: null, right: null };
+  this.right = { el: null, left: null, right: null };
+}
+
+function BST() {
+  this.root = new Node(null);
+}
+
+BST.prototype.insert = function(el, root) {
+  root = root || this.root;
+
+  if (root.el === null) {
+    root.el = el;
+    root.left = { el: null, left: null, right: null };
+    root.right = { el: null, left: null, right: null };
+  }
+  else if (el < root.el) {
+    this.insert(el, root.left);
+  }
+  else if (el > root.el) {
+    this.insert(el, root.right);
+  }
+};
+
+BST.prototype.find = function(el, root) {
+  root = root || this.root;
+
+  if (root.el === null) {
+    return false;
+  }
+
+  if (el === root.el) {
+    return true;
+  }
+  else if (el < root.el) {
+    return this.find(el, root.left);
+  }
+  else if (el > root.el) {
+    return this.find(el, root.right);
+  }
+};
+
+BST.prototype.levelorder = function(cb) {
+  var curr, queue = [];
+  queue.push(this.root);
+
+  while (queue.length > 0) {
+    curr = queue.shift();
+    cb(curr.el);
+
+    if (curr.left.el) {
+      queue.push(curr.left);
+    }
+    if (curr.right.el) {
+      queue.push(curr.right);
+    }
+  }
+};
+
+BST.prototype.preorder = function(cb, curr) {
+  curr = curr || this.root;
+
+  if (curr.el === null) {
+    return;
+  }
+
+  cb(curr.el);
+  this.preorder(cb, curr.left);
+  this.preorder(cb, curr.right);
+};
+
+BST.prototype.inorder = function(cb, curr) {
+  curr = curr || this.root;
+
+  if (curr.el === null) {
+    return;
+  }
+
+  this.inorder(cb, curr.left);
+  cb(curr.el);
+  this.inorder(cb, curr.right);
+};
+
+BST.prototype.postorder = function(cb, curr) {
+  curr = curr || this.root;
+
+  if (curr.el === null) {
+    return;
+  }
+
+  this.postorder(cb, curr.left);
+  this.postorder(cb, curr.right);
+  cb(curr.el);
+};
+
+BST.prototype.min = function(curr) {
+  curr = curr || this.root;
+  if (curr.left.el === null) {
+    return curr.el;
+  }
+  return this.min(curr.left);
+};
+
+BST.prototype.max = function(curr) {
+  curr = curr || this.root;
+  if (curr.right.el === null) {
+    return curr.el;
+  }
+  return this.max(curr.right);
+};
+
+BST.prototype.height = function(curr) {
+  curr = curr || this.root;
+
+  if (curr.el === null) {
+    return 0;
+  }
+
+  var left = this.height(curr.left);
+  var right = this.height(curr.right);
+
+  return 1 + Math.max(left, right);
+};
+
+BST.prototype.remove = function(el) {
+  var nodeToRemove, parentDirection, singleChildDirection, minRightSubtree;
+  var parent = this._findParent(el);
+  if (!parent) {
+    if (el === this.root.el) {
+      if (this.root.left.el === null && this.root.right.el === null) {
+        this.root = new Node(null);
+      }
+      else if (this.root.left.el && this.root.right.el) {
+        minRightSubtree = this.min(this.root.right);
+        this.remove(minRightSubtree);
+        this.root.el = minRightSubtree;
+      }
+      else {
+        if (this.root.left) {
+          this.root = this.root.left;
+        }
+        else if (this.root.right) {
+          this.root = this.root.right
+        }
+      }
+      return el;
+    }
+    else {
+      return false;
+    }
+  }
+
+  if (parent.left.el === el) {
+    nodeToRemove = parent.left;
+    parentDirection = 'left';
+  }
+  else if (parent.right.el === el) {
+    nodeToRemove = parent.right;
+    parentDirection = 'right';
+  }
+
+  // leaf node
+  if (nodeToRemove.left.el === null && nodeToRemove.right.el === null) {
+    parent[parentDirection] = { el: null, left: null, right: null };
+    return el;
+  }
+
+  // two children
+  else if (nodeToRemove.left.el && nodeToRemove.right.el) {
+    minRightSubtree = this.min(nodeToRemove.right);
+    this.remove(minRightSubtree);
+    nodeToRemove.el = minRightSubtree;
+    return el;
+  }
+
+  // one child (left)
+  else {
+    if (nodeToRemove.left.el) {
+      singleChildDirection = 'left';
+    }
+    else if (nodeToRemove.right.el) {
+      singleChildDirection = 'right';
+    }
+
+    parent[parentDirection] = nodeToRemove[singleChildDirection];
+    return el;
+  }
+};
+
+BST.prototype._findParent = function(el, curr) {
+  curr = curr || this.root;
+  if (curr.el === null || curr.el === el) {
+    return false;
+  }
+  else if (curr.left.el === el || curr.right.el === el) {
+    return curr;
+  }
+  else if (el < curr.el) {
+    return this._findParent(el, curr.left);
+  }
+  else if (el > curr.el) {
+    return this._findParent(el, curr.right);
+  }
+};
---
+++ dataStructures/binarySearchTree/binarySearchTreeSpec.js
@@ -0,0 +1,297 @@
+describe('BST', function() {
+  var bst;
+
+  beforeEach(function() {
+    bst = new BST();
+  });
+
+  describe('#insert and #find', function() { // hard to decouple the two
+    describe('when empty', function() {
+      it("can't find an element when empty", function() {
+        expect(bst.find(5)).toBe(false);
+      });
+
+      it('can insert and find an element', function() {
+        bst.insert(5);
+        expect(bst.find(5)).toBe(true);
+      });
+
+      it("can't find an element that isn't there after inserting", function() {
+        bst.insert(5);
+        expect(bst.find(10)).toBe(false);
+      });
+    });
+
+    describe('with a root element', function() {
+      beforeEach(function() {
+        bst.insert(5);
+      });
+
+      it('can insert and find a smaller element', function() {
+        bst.insert(3);
+        expect(bst.find(3)).toBe(true);
+      });
+
+      it('can insert and find an equal element', function() {
+        bst.insert(3);
+        expect(bst.find(3)).toBe(true);
+      });
+
+      it('can insert and find a larger element', function() {
+        bst.insert(7)
+        expect(bst.find(7)).toBe(true);
+      });
+
+      it("can't find an element that isn't there", function() {
+        expect(bst.find(10)).toBe(false);
+      });
+    });
+
+    describe('root-left', function() {
+      /*
+                      5
+                    3
+      */
+      beforeEach(function() {
+        bst.insert(5);
+        bst.insert(3);
+      });
+
+      describe('can insert and find an element', function() {
+        it('larger than the root', function() {
+          bst.insert(6);
+          expect(bst.find(6)).toBe(true);
+        });
+
+        it('equal to the root', function() {
+          bst.insert(5);
+          expect(bst.find(5)).toBe(true);
+        });
+
+        it('between the root and the left child', function() {
+          bst.insert(4);
+          expect(bst.find(4)).toBe(true);
+        });
+
+        it('equal to the left child', function() {
+          bst.insert(3);
+          expect(bst.find(3)).toBe(true);
+        });
+
+        it('less than the left child', function() {
+          bst.insert(2);
+          expect(bst.find(2)).toBe(true);
+        });
+      });
+    });
+
+    describe('root-right', function() {
+      /*
+                      5
+                        7
+      */
+      beforeEach(function() {
+        bst.insert(5);
+        bst.insert(7);
+      });
+
+      describe('can insert and find an element', function() {
+        it('less than the root', function() {
+          bst.insert(4);
+          expect(bst.find(4)).toBe(true);
+        });
+
+        it('equal to the root', function() {
+          bst.insert(5);
+          expect(bst.find(5)).toBe(true);
+        });
+
+        it('between the root and the right child', function() {
+          bst.insert(6);
+          expect(bst.find(6)).toBe(true);
+        });
+
+        it('equal to the right child', function() {
+          bst.insert(7);
+          expect(bst.find(7)).toBe(true);
+        });
+
+        it('greater than the right child', function() {
+          bst.insert(8);
+          expect(bst.find(8)).toBe(true);
+        });
+      });
+    });
+  });
+
+  describe('traversal', function() {
+    /*
+                    4
+                  2   6
+                1  3 5  7
+    */
+    beforeEach(function() {
+      spy = jasmine.createSpy();
+      bst.insert(4);
+      bst.insert(2);
+      bst.insert(6);
+      bst.insert(1);
+      bst.insert(3);
+      bst.insert(5);
+      bst.insert(7);
+    });
+
+    describe('breadth first', function() {
+      it('#levelorder', function() {
+        bst.levelorder(spy);
+        expect(spy.calls[0].args[0]).toBe(4);
+        expect(spy.calls[1].args[0]).toBe(2);
+        expect(spy.calls[2].args[0]).toBe(6);
+        expect(spy.calls[3].args[0]).toBe(1);
+        expect(spy.calls[4].args[0]).toBe(3);
+        expect(spy.calls[5].args[0]).toBe(5);
+        expect(spy.calls[6].args[0]).toBe(7);
+      });
+    })
+
+    describe('depth first', function() {
+      // root-left-right
+      it('#preorder', function() {
+        bst.preorder(spy);
+        expect(spy.calls[0].args[0]).toBe(4);
+        expect(spy.calls[1].args[0]).toBe(2);
+        expect(spy.calls[2].args[0]).toBe(1);
+        expect(spy.calls[3].args[0]).toBe(3);
+        expect(spy.calls[4].args[0]).toBe(6);
+        expect(spy.calls[5].args[0]).toBe(5);
+        expect(spy.calls[6].args[0]).toBe(7);
+      });
+
+      // left-root-right
+      it('#inorder', function() {
+        bst.inorder(spy);
+        expect(spy.calls[0].args[0]).toBe(1);
+        expect(spy.calls[1].args[0]).toBe(2);
+        expect(spy.calls[2].args[0]).toBe(3);
+        expect(spy.calls[3].args[0]).toBe(4);
+        expect(spy.calls[4].args[0]).toBe(5);
+        expect(spy.calls[5].args[0]).toBe(6);
+        expect(spy.calls[6].args[0]).toBe(7);
+      });
+
+      // left-right-root
+      it('#postorder', function() {
+        bst.postorder(spy);
+        expect(spy.calls[0].args[0]).toBe(1);
+        expect(spy.calls[1].args[0]).toBe(3);
+        expect(spy.calls[2].args[0]).toBe(2);
+        expect(spy.calls[3].args[0]).toBe(5);
+        expect(spy.calls[4].args[0]).toBe(7);
+        expect(spy.calls[5].args[0]).toBe(6);
+        expect(spy.calls[6].args[0]).toBe(4);
+      });
+    });
+  });
+
+  describe('other', function() {
+    /*
+                    4
+                  2   6
+                1  3 5  7
+    */
+    beforeEach(function() {
+      bst.insert(4);
+      bst.insert(2);
+      bst.insert(6);
+      bst.insert(1);
+      bst.insert(3);
+      bst.insert(5);
+      bst.insert(7);
+    });
+
+    it('#min', function() {
+      expect(bst.min()).toBe(1);
+    });
+
+    it('#max', function() {
+      expect(bst.max()).toBe(7);
+    });
+
+    it('#height', function() {
+      expect(bst.height()).toBe(3);
+    });
+  });
+
+  xdescribe('#isbst', function() {
+
+  });
+
+  describe('#remove', function() {
+    /*
+              4
+            2   6
+          1    5  7
+    */
+    beforeEach(function() {
+      bst.insert(4);
+      bst.insert(2);
+      bst.insert(6);
+      bst.insert(1);
+      bst.insert(5);
+      bst.insert(7);
+    });
+
+    it("can't remove an element that doesn't exist", function() {
+      expect(bst.remove(10)).toBe(false);
+    });
+
+    it('can remove a leaf node', function() {
+      expect(bst.remove(1)).toBe(1);
+      expect(bst.find(1)).toBe(false);
+    });
+
+    it('can remove a node with one child', function() {
+      expect(bst.remove(2)).toBe(2);
+      expect(bst.find(2)).toBe(false);
+    });
+
+    describe('can remove a node with two children', function() {
+      it('when the right subtree minimum is the root', function() {
+        expect(bst.remove(6)).toBe(6);
+        expect(bst.find(6)).toBe(false);
+      });
+
+      it('when the right subtree minimum is on the left', function() {
+        bst.insert(6.5);
+        expect(bst.remove(6)).toBe(6);
+        expect(bst.find(6)).toBe(false);
+      });
+    });
+
+    describe('can deal with the root node', function() {
+      it('with zero children', function() {
+        bst = new BST();
+        bst.insert(1);
+        expect(bst.remove(1)).toBe(1);
+        expect(bst.find(1)).toBe(false);
+      });
+
+      it('with one child', function() {
+        bst = new BST();
+        bst.insert(1);
+        bst.insert(2);
+        expect(bst.remove(1)).toBe(1);
+        expect(bst.find(1)).toBe(false);
+      });
+
+      it('with two children', function() {
+        bst = new BST();
+        bst.insert(1);
+        bst.insert(0);
+        bst.insert(2);
+        expect(bst.remove(1)).toBe(1);
+        expect(bst.find(1)).toBe(false);
+      });
+    });
+  });
+});
